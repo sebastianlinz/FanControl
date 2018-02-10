24363f47268b8f545792957ea8cad4ec0d9beb99
Sebastian Linz <sebastian_linz@yahoo.de>
2018-02-10 19:54:11 +0100

    Comments fixed.

--- config.js
+++ config.js
@@ -6,18 +6,18 @@ module.exports.password = "";
 // numbers are the playerId.)
 module.exports.playerId = "";
 
-// Zwift simuation mode, level to switch fan to 1st level
+// Zwift simuation mode: speed to switch fan to 1st level
 module.exports.speedLevel1 = 10;
-// Zwift simuation mode, level to switch fan to 2nd level
+// Zwift simuation mode: speed to switch fan to 2nd level
 module.exports.speedLevel2 = 30;
-// Zwift simuation mode, level to switch fan to 3rd level
+// Zwift simuation mode: speed to switch fan to 3rd level
 module.exports.speedLevel3 = 40;
 
-// Zwift workout mode, fan is only switched on if above this heartrate
+// Zwift workout mode: fan is only switched on if above this heartrate
 module.exports.heartrate = 125;
-//   in workout mode, fan to 1st level if above this power level
+//   in workout mode: fan to 1st level if above this power level
 module.exports.powerLevel1 = 150;   // eg. 55% of FTP
-//   in workout mode, fan to 2nd level if above this power level
+//   in workout mode: fan to 2nd level if above this power level
 module.exports.powerLevel2 = 195;   // eg. 75% of FTP
-//   in workout mode, fan to 3rd level if above this power level
+//   in workout mode: fan to 3rd level if above this power level
 module.exports.powerLevel3 = 265;   // eg. 95% of FTP
