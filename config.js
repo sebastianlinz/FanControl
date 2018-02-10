4176299e036641866464242fab82751339c67a2f
Sebastian Linz <sebastian_linz@yahoo.de>
2018-02-10 19:50:10 +0100

    configuration file commented.

--- config.js
+++ config.js
@@ -1,14 +1,23 @@
-
+// Zwift username
 module.exports.username = "";
+// Zwift password
 module.exports.password = "";
-module.exports.playerId = "";
+// Zwift player id (find the playerId on your PC running the Zwift client. There is a folder named userXXXXXX. These
+// numbers are the playerId.)
+module.exports.playerId = "209986";
 
-module.exports.speedLevel1 = 6;
-module.exports.speedLevel2 = 24;
+// Zwift simuation mode, level to switch fan to 1st level
+module.exports.speedLevel1 = 10;
+// Zwift simuation mode, level to switch fan to 2nd level
+module.exports.speedLevel2 = 30;
+// Zwift simuation mode, level to switch fan to 3rd level
 module.exports.speedLevel3 = 40;
 
+// Zwift workout mode, fan is only switched on if above this heartrate
 module.exports.heartrate = 125;
-
-module.exports.powerLevel1 = 160;   // 55%
-module.exports.powerLevel2 = 218;   // 75%
-module.exports.powerLevel3 = 276;   // 95%
+//   in workout mode, fan to 1st level if above this power level
+module.exports.powerLevel1 = 150;   // eg. 55% of FTP
+//   in workout mode, fan to 2nd level if above this power level
+module.exports.powerLevel2 = 195;   // eg. 75% of FTP
+//   in workout mode, fan to 3rd level if above this power level
+module.exports.powerLevel3 = 265;   // eg. 95% of FTP
